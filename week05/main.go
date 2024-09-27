2f86b159a0ed0551327757297529726831b75333
0215dbwkd <0215dbwkd@naver.com>
2024-09-27 18:42:39 +0900

    v0.3 IO & Types

--- week05/main.go
+++ week05/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt" // c언어의 #include <stdio.h>
+import (
+	"fmt" // c언어의 #include <stdio.h>
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
 	// var i int
@@ -8,13 +13,19 @@ func main() {
 
 	//var i int = 55
 
-	i := 55 //:=단축 연산자 var 안써도 괜찮은 타입도 안적음(뒤에 오는걸로 추적)
-	var f float32 = 1.92
+	var f float64 = 1.92
 
+	//f := 1.92
+	i := "55" //:=단축 연산자 var 안써도 괜찮은 타입도 안적음(뒤에 오는걸로 추적)
+
+	fmt.Println(strings.Title("kim inha"))
+	fmt.Printf("%f %f %f\n", f, math.Ceil(f), math.Floor(f))
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
 	fmt.Println("f is", f)
 	fmt.Println("i is", i)
 	fmt.Println("i is", i)
 	fmt.Print("i is ", i, "\n")
-	fmt.Printf("i is %d\n", i)
+	//fmt.Printf("i is %d\n", i)
+	fmt.Printf("i is %s\n", i)
 
 }
