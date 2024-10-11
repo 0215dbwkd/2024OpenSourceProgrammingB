203cff69bb8cf3c77bd00fc15df7248a343b5096
0215dbwkd <0215dbwkd@naver.com>
2024-10-11 18:17:17 +0900

    v1.2 Parse

--- week07/main.go
+++ week07/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	var float32 float32 = 9.1
-	fmt.Println(float32)
-	var number float32
-	fmt.Println(number)
+	fmt.Print("점수 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	score, err := in.ReadString('\n') //err 대신 _를 쓰면 print에 err안써도 됨
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Print(strings.TrimSpace(score))
+
+	score = strings.TrimSpace(score)              //줄 바꿈, 띄어쓰기, 탭 등 제거 (python strip과 유사)
+	realScore, _ := strconv.ParseFloat(score, 64) //실수형 64비트 타입으로 형변환
+
+	if realScore >= 90 {
+		fmt.Println("A")
+	} else {
+		fmt.Println("BCDF")
+	}
+
 }
