1c8ff1df4d1e0baca6967a31ae7f4e82d2bfd1c0
0215dbwkd <0215dbwkd@naver.com>
2024-10-11 16:57:58 +0900

    0.8 Replace

--- week07/main.go
+++ week07/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	// year := now.Year()
-	// month := now.Month()
-	// day := now.Day()
-	// fmt.Println(int(year), "년", int(month), "월", int(day), "일 입니다")
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", now.Year(), int(now.Month()), now.Day())
-	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
+	// var now time.Time = time.Now()
+	// // year := now.Year()
+	// // month := now.Month()
+	// // day := now.Day()
+	// // fmt.Println(int(year), "년", int(month), "월", int(day), "일 입니다")
+	// fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", now.Year(), int(now.Month()), now.Day())
+	// fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
+
+	var army string = "우리는 !가와 !민에 충성을 다하는 대한민! 육!이다."
+	armyFixed := strings.NewReplacer("!", "국")
+	fmt.Println(army)
 }
