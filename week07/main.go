b5fd2ff71112d54ef507928c2fc211140f174abe
0215dbwkd <0215dbwkd@naver.com>
2024-10-11 17:23:28 +0900

    v1.0 If

--- week07/main.go
+++ week07/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("이름 입력 : ")
 	r := bufio.NewReader(os.Stdin)
-	name, err := r.ReadString('\n')
-	fmt.Println(name)
-	fmt.Println(err)
+	name, err := r.ReadString('\n') //err 대신 _를 쓰면 print에 err안써도 됨
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
+
 }
