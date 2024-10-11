2eca14c4b0da663128932dbbaec72d0c1c9989f0
0215dbwkd <0215dbwkd@naver.com>
2024-10-11 18:26:00 +0900

    v1.3 ParseInt, Base16

--- week07/main.go
+++ week07/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 func main() {
+	// var realScore int32
 	fmt.Print("점수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	score, err := in.ReadString('\n') //err 대신 _를 쓰면 print에 err안써도 됨
@@ -19,13 +20,16 @@ func main() {
 	}
 	// fmt.Print(strings.TrimSpace(score))
 
-	score = strings.TrimSpace(score)              //줄 바꿈, 띄어쓰기, 탭 등 제거 (python strip과 유사)
-	realScore, _ := strconv.ParseFloat(score, 64) //실수형 64비트 타입으로 형변환
+	score = strings.TrimSpace(score)                 //줄 바꿈, 띄어쓰기, 탭 등 제거 (python strip과 유사)
+	realScore, _ := strconv.ParseInt(score, 16., 32) //정수형 32비트 타입으로 형변환
 
-	if realScore >= 90 {
+	if realScore >= 60 {
+
 		fmt.Println("A")
+		fmt.Printf("%d\n", realScore)
 	} else {
 		fmt.Println("BCDF")
+		fmt.Printf("%d\n", realScore)
 	}
 
 }
