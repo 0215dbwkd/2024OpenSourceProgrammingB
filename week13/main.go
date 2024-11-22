cc0f36fbda9f81cf23c0b4d7c9b90d0fae8fb9aa
0215dbwkd <0215dbwkd@naver.com>
2024-11-22 18:25:38 +0900

    v4.5

--- week13/main.go
+++ week13/main.go
@@ -1,22 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+// func test(strs string){
+// func test(strs ...string,i int) //error
+func test(i int, strs ...string) {
+	fmt.Println(i, strs, reflect.TypeOf(strs))
+}
 
 func main() {
-	var emptySlice []bool
-	//emptySlice = make([]bool, 5)
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // slice zero value (nil)
-	if len(emptySlice) == 0 {
-		emptySlice = append(emptySlice, true)
-	}
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // []bool{true}, 1
-
-	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} //array := array literal
-	gpa_slice := gpas[1:4]                       // [4.1, 4.5, 3.9]
-	//gpa_slice[1] = 2.71
-	gpas[1] = 2.71
-	//gpa_slice = append(gpa_slice, 4.3)
-	gpa_slice = append(gpa_slice, 4.3, 5.55)
-	fmt.Println(len(gpa_slice), gpa_slice, gpas)
+	//fmt.Println(os.Args, len(os.Args), reflect.TypeOf(os.Args))
+	slices := os.Args[1:]
+	fmt.Println(slices, slices[1])
+	test(1, "123")
+	test(-99, "123", "abc")
+	test(55)
+	test(0, "123", "abc", "123a")
 
 }
